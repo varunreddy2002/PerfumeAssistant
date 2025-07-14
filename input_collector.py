51634b8a7b8a1da33f9748e292f454640a7654ac
Krishna-Shivkumar <krishna.shivkumar8421@gmail.com>
2025-07-14 13:44:19 -0400

    updated questions

--- input_collector.py
+++ input_collector.py
@@ -5,6 +5,20 @@ def collect_user_input():
 
     gender = input("Preferred gender profile for the scent (feminine, masculine, unisex): ").strip().lower()
 
+    a = 0
+    while(a==0):
+        print("\nEnter the user's age:")
+        a1 = input("Strictly write the answer as a number:")
+        try:
+            age = int(a1)
+            a = 1
+        except:
+            print("The given response was not in the correct type, please give a new response.")
+
+    country = input("What is the preferred perfume country of production: ").strip().lower()
+
+    brand = input("Perfume Brand preference: ").strip().lower()
+
     print("\nEnter 2–3 specific scent notes you prefer (e.g., vanilla, rose, citrus):")
     notes_raw = input("Separate each with a comma: ")
     preferred_notes = [n.strip().lower() for n in notes_raw.split(",")]
@@ -13,12 +27,35 @@ def collect_user_input():
     accords_raw = input("Separate each with a comma: ")
     preferred_accords = [a.strip().lower() for a in accords_raw.split(",")]
 
-    free_text = input("\nOptional: Describe what you're looking for in your own words (or leave blank): ")
+    t= 0
+    while(t==0):
+        print("\nEnter the expected temperature for the day:")
+        temperature = input("Strictly write the answer as a number, in fahrenheit:")
+        try:
+            temp = int(temperature)
+            t = 1
+        except:
+            print("The given response was not in the correct type, please give a new response.")
+    
+    mood = input("\nWhat mood do you want to evoke (e.g., bold, sincere, emo, romantic): ")
+
+    occasion = input("\nWhat occasion is the perfume for:")
+
+    humidity = input("Level of expected humidity (high, medium, low, none): ").strip().lower()
+
+    free_text = input("\nList any futher descriptions/parameters for the perfume: ")
 
     return {
         "gender": gender,
+        "age":age,
+        "country":country,
+        "brand":brand,
         "preferred_notes": preferred_notes,
         "preferred_accords": preferred_accords,
+        "temperature":temp,
+        "mood":mood,
+        'occasion':occasion,
+        'humidity':humidity,
         "free_text": free_text
     }
 
@@ -46,7 +83,6 @@ if os.path.exists(user_input_file):
 else:
     all_inputs = []
 
-# Append new input
 all_inputs.append(user_data)
 
 # Save back to file
